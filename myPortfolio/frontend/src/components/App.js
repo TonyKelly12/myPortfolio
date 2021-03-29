a494dca0c9423b30857f67deca5773300e2ea0a9
TonyKelly12 <tonykelly1286@gmail.com>
2021-03-29 16:32:12 -0500

    init layout

--- myPortfolio/frontend/src/components/App.js
+++ myPortfolio/frontend/src/components/App.js
@@ -1,34 +1,82 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-import Footer from './layout/footer/footer';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
-import Header from './layout/header/header'
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+
+import Footer from "./layout/footer/footer";
+import Header from "./layout/header/header";
+import WorkCard from "./card/work-card";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Container fluid>
-                    <Row>
-                        <Col>
-                        <Header/>
-                        <Container fluid>
+  render() {
+    return (
+      <div>
+        <ParallaxProvider>
+          <Row style={{ position: "absolute", width: "100%" }}>
+            <Header />
+            <Col xs={3}  style={{ marginRight: "1%" }}>
+              <Container
+                style={{ width: "30%", alignContent: "left", margin: 0 }}
+              >
+                <Jumbotron>
+                  <h1>Hello, world!</h1>
+                  <p>
+                    This is a simple hero unit, a simple jumbotron-style
+                    component for calling extra attention to featured content or
+                    information.
+                  </p>
+                  <p>
+                    <Button variant="primary">Learn more</Button>
+                  </p>
+                </Jumbotron>
+                <Header />
+              </Container>
+            </Col>
+            <Col xs={6}>
+              <ListGroup
+                className="overflow-auto"
+                style={{ position: "absolute", height: "100vh", width: '100%' }}
+              >
+                <ListGroup.Item
+                  style={{ height: "70%", width: "80%", margin: 50 }}
+                >
+                  <WorkCard />
+                </ListGroup.Item>
 
-                        </Container>
-                        </Col>
-                        <Col>1 of 1</Col>
-                    </Row>
-                </Container>
-                <Footer />
-            </div>
+                <ListGroup.Item
+                  style={{ height: "70%", width: "80%", margin: 50 }}
+                >
+                  <WorkCard />
+                </ListGroup.Item>
 
-        )
-
-    }
+                <ListGroup.Item
+                  style={{ height: "70%", width: "80%", margin: 50 }}
+                >
+                  <WorkCard />
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Footer
+              style={{
+                position: "fixed",
+                bottom: "0",
+                left: "0",
+                width: "100%",
+              }}
+            />
+          </Row>
+        </ParallaxProvider>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
--- myPortfolio/frontend/src/components/layout/footer/footer.js
+++ myPortfolio/frontend/src/components/layout/footer/footer.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export class Footer extends Component {
-    render() {
-        return (
-          <Navbar  bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand href="#home">Hi, Im Tony Kelly</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        )
-    }
+  render() {
+    return (
+      <div style={phantom}>
+        <div style={style}>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="#home">Hi, Im Tony Kelly</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
+
+var style = {
+  
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  width: "100%",
+};
+
+var phantom = {
+  display: "block",
+ 
+  height: "60px",
+  width: "100%",
+};
