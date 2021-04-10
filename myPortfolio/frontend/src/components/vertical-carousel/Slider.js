c70e9beecc606dd2bd762a0a022da939057b3453
TonyKelly12 <tonykelly1286@gmail.com>
2021-04-10 17:51:27 -0500

    added gesture to photo

--- myPortfolio/frontend/src/components/vertical-carousel/Slider.js
+++ myPortfolio/frontend/src/components/vertical-carousel/Slider.js
@@ -1,84 +1,108 @@
-import React, { useState } from 'react'
-import { useSpring, animated, interpolate } from 'react-spring'
-import { useGesture } from 'react-with-gesture'
-import { AiFillCaretDown, AiFillCaretUp} from "react-icons/ai";
-import SlideItems from './items'; 
-import './styles.css'
-import { range } from 'lodash';
+import React, { useState } from "react";
+import { useSpring, animated, interpolate } from "react-spring";
+import { useGesture } from "react-with-gesture";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
+import SlideItems from "./items";
+import "./styles.css";
+import { range } from "lodash";
 
 const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
-  const [nxt, setNextState] = useState(false);
-  const [prev, setPrevState] = useState(false);
+ 
   const length = slides.length;
 
-  const { up } = useSpring({
-    up: nxt ? 1 : 0,
+  const [{ y }, set] = useSpring(() => ({ y: 0 }))
+  const bind = useGesture (( { down, delta, first}) => {
+    if(delta[1] <= 400 && delta[1] >= -400){
+      console.log('delta', delta[1])
+    }
+    
+    
+    set({ y: down ? delta[1] : 0 });
+
+    if ( first && delta[1] <= -200 ){
+      console.log('next called', current)
+      console.log('event', event)
+      setCurrent(current => current === length - 1 ? 0 : current + 1);
+      console.log('current', current)
+      
+    }else if ( first && delta[1]  >= 200) {
+      console.log('previous called', current)
+    setCurrent(current => current === 0 ? length - 1 : current - 1);
+    console.log('current', current)
+    
+    }
+
+    
   })
 
-  const nextImg = () =>{ 
-    resetState();
-    setNextState(true);
-    setPrevState(false);
-    setCurrent( current === length -1 ? 0 : current + 1);
-   
-  }
+  const nextImg = () => {
+    console.log('next called', current)
+    console.log('length', length)
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    console.log('current', current)
+  };
 
-  const resetState = () => {
-    setNextState(false);
-    setPrevState(false);
-  }
+  const previousImg = () => {
+    console.log('previous called', current)
+    setCurrent(current === 0 ? length - 1 : current - 1);
+    console.log('current', current)
+  };
 
-  const previousImg = () =>{
-    resetState();
-    setNextState(false);
-    setPrevState(true);
-    setCurrent(current === 0 ? length - 1 : current -1)
-  }
-
-  if(!Array.isArray(slides) || slides.length <= 0) {
+  if (!Array.isArray(slides) || slides.length <= 0) {
     // TODO: add default image
     return null;
   }
 
- 
-  
- 
-
   return (
-    <section className='slider'>
-      <AiFillCaretDown className='down-arrow' onClick={previousImg}/>
-      <AiFillCaretUp className='up-arrow' onClick={nextImg}/>
-      {SlideItems.map((slide, index) =>{
+    <section className="slider">
+      <AiFillCaretDown className="down-arrow" onClick={previousImg} />
+      <AiFillCaretUp className="up-arrow" onClick={nextImg} />
+      {SlideItems.map((slide, index) => {
         return (
           <animated.div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index} style={{
-              pointerEvents: 'all'
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+            style={{
+              pointerEvents: "all",
             }}
-            >
-              {index === current && (
-              
-               
-                
-                 <animated.img  src={slide.url} alt='add alt txt'className="fg" 
-                 style={{
-              transform: up.interpolate({
-                range: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1],
-                output: [0, 30, 60, 90, 120,-120, -90, ,-60, -30,  0]
-              }).interpolate( up => `translateY(${up}%)`),
-            }}
-                 
-                 />
-                
-               
-                
-              )}
-            </animated.div>
-        
-      )})}
-    </section> 
-  )
-}
+          >
+            {index === current && (
+              <animated.img
+              style={{
+                opacity: y.interpolate({
+                  map: Math.abs,
+                  range: [0,300],
+                  output: [1, 0], 
+                  extrapolate: 'clamp'
+                }),
+                transform: y.interpolate((y) => 
+                `translate3d(0, ${y}px, 0)`),
+                zIndex: 25
+              }}
+              {...bind()}
+                src={slide.url}
+                alt="add alt txt"
+                className="fg"
 
-export default ImageSlider
+              />
+            )}
+          </animated.div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default ImageSlider;
+
+
+// const throwAway = {{
+//   transform: up
+//     .interpolate({
+//       map: Math.abs,
+//       range: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1],
+//       output: [0, 30, 60, 90, 120, -120, -90, , -60, -30, 120],
+//       extrapolate: "clamp",
+//     })
+//     .interpolate((up) => `translateY(${up}%)`),
